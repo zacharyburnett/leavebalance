509f930d425b8e00812bb5994fd440c2e6bf3899
zacharyburnett <zachary.r.burnett@gmail.com>
2023-06-02 15:32:02 -0400

    split into subcommands

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,16 +1,36 @@
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize, Default)]
 pub struct Configuration {
     pub policy: LeavePolicy,
     #[serde(default)]
     pub planned_leave: Vec<(chrono::NaiveDate, chrono::NaiveDate)>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, serde::Serialize)]
 pub struct LeavePolicy {
     pub working_hours: (chrono::NaiveTime, chrono::NaiveTime),
     pub working_days: Vec<chrono::Weekday>,
     pub days_in_pay_period: u32,
     pub hours_accrued_per_pay_period: f64,
+}
+
+impl Default for LeavePolicy {
+    fn default() -> Self {
+        Self {
+            working_hours: (
+                chrono::NaiveTime::from_hms_opt(9, 0, 0).unwrap(),
+                chrono::NaiveTime::from_hms_opt(17, 0, 0).unwrap(),
+            ),
+            working_days: vec![
+                chrono::Weekday::Mon,
+                chrono::Weekday::Tue,
+                chrono::Weekday::Wed,
+                chrono::Weekday::Thu,
+                chrono::Weekday::Fri,
+            ],
+            days_in_pay_period: 14,
+            hours_accrued_per_pay_period: 8.0,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -28,6 +48,12 @@ mod tests {
         let file = std::fs::File::open(path).unwrap();
         let configuration: Configuration = serde_yaml::from_reader(file).unwrap();
 
-        assert_eq!(configuration.policy.working_day_hours, 8.0);
+        assert_eq!(
+            configuration.policy.working_hours,
+            (
+                chrono::NaiveTime::from_hms_opt(8, 0, 0).unwrap(),
+                chrono::NaiveTime::from_hms_opt(16, 0, 0).unwrap()
+            )
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,83 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    // future date to calculate leave for
-    on: chrono::NaiveDate,
-    // path to configuration file
-    configuration: std::path::PathBuf,
-    // current leave balance in hours
-    current_leave_balance: Option<f64>,
-    // next pay day
-    next_pay_day: Option<chrono::NaiveDate>,
-    // balance threshold under which to warn
-    balance_warn_threshold: Option<u32>,
-    #[clap(short, long)]
-    verbose: bool,
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    On {
+        // future date to calculate leave for
+        on: chrono::NaiveDate,
+        // path to configuration file (create one with ```leavebalance config`)
+        configuration: std::path::PathBuf,
+        // current leave balance in hours
+        current_leave_balance: Option<f64>,
+        // next pay day
+        next_pay_day: Option<chrono::NaiveDate>,
+        // balance threshold under which to warn
+        balance_warn_threshold: Option<u32>,
+        #[clap(short, long)]
+        verbose: bool,
+    },
+    Config {
+        // path to configuration file to write
+        filename: std::path::PathBuf,
+        hours_accrued_per_pay_period: f64,
+    },
 }
 
 fn main() {
     let arguments = Cli::parse();
 
-    let file = std::fs::File::open(arguments.configuration).unwrap();
-    let configuration: configuration::Configuration = serde_yaml::from_reader(file).unwrap();
+    match arguments.command {
+        Command::On {
+            on,
+            configuration,
+            current_leave_balance,
+            next_pay_day,
+            balance_warn_threshold,
+            verbose,
+        } => {
+            let file = std::fs::File::open(configuration).unwrap();
+            let configuration: configuration::Configuration =
+                serde_yaml::from_reader(file).unwrap();
 
-    let working_time = configuration.policy.working_hours.1 - configuration.policy.working_hours.0;
+            let working_time =
+                configuration.policy.working_hours.1 - configuration.policy.working_hours.0;
 
-    let balance = balance::balance_on(
-        arguments.on,
-        configuration.policy,
-        arguments.next_pay_day,
-        arguments.current_leave_balance,
-        configuration.planned_leave,
-        arguments.balance_warn_threshold,
-        arguments.verbose,
-    );
+            let balance = balance::balance_on(
+                on,
+                configuration.policy,
+                next_pay_day,
+                current_leave_balance,
+                configuration.planned_leave,
+                balance_warn_threshold,
+                verbose,
+            );
 
-    println!(
-        "your leave balance will be {:} hours ({:} working days) on {:}",
-        balance.num_hours(),
-        balance.num_hours() / working_time.num_hours(),
-        arguments.on,
-    );
+            println!(
+                "your leave balance will be {:} hours ({:} working days) on {:}",
+                balance.num_hours(),
+                balance.num_hours() / working_time.num_hours(),
+                on,
+            );
+        }
+        Command::Config {
+            hours_accrued_per_pay_period,
+            filename,
+        } => {
+            let mut configuration = configuration::Configuration::default();
+            configuration.policy.hours_accrued_per_pay_period = hours_accrued_per_pay_period;
+
+            let file = std::fs::File::create(filename).unwrap();
+            match serde_yaml::to_writer(file, &configuration) {
+                Ok(_) => {}
+                Err(error) => {
+                    panic!("{:}", error);
+                }
+            }
+        }
+    }
 }
